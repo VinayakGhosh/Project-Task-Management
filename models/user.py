6086732beb59acbf32f0688e372fa2168e03b367
Vinayak Ghosh <vinayak@embetter.in>
2026-01-01 02:40:43 +0530

    added subscription model

--- models/user.py
+++ models/user.py
@@ -15,4 +15,16 @@ class Users(Base):
     hashed_password = Column(String, nullable=False)
     CreatedAtTimeStamp = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
     UpdatedAtTimeStamp = Column(TIMESTAMP(timezone=True), onupdate=text('now()'), server_default=text('now()'))
+
+
+class Subscriptions(Base):
+    __tablename__ = "subscriptions"
+    subscription_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID, ForeignKey("users.user_id"), nullable=False)
+    plan_id = Column(UUID, ForeignKey("plans.plan_id"), nullable=False)
+    start_timestamp = Column(TIMESTAMP(timezone=True), nullable=False)
+    end_timestamp = Column(TIMESTAMP(timezone=True), nullable=False)
+    status = Column(String, nullable=False)
+    CreatedAtTimeStamp = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
+    UpdatedAtTimeStamp = Column(TIMESTAMP(timezone=True), onupdate=text('now()'), server_default=text('now()'))
     
