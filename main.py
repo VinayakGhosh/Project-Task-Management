8234d4da4afc52065a761e20808d34ed91ab174b
Vinayak Ghosh <r.vinayakghosh@gmail.com>
2026-04-14 16:40:22 +0530

    version bump

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ async def lifespan(app:FastAPI):
 app = FastAPI(
     title="Proj_Task",
     description="api for proj_task",
-    version="0.3.3",
+    version="0.3.4",
     lifespan=lifespan
 )
 
